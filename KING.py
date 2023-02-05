8dee24bc39ed49100199b3c8435b129fd7f0412e
D1kaseem <66321036+dhiqnazi@users.noreply.github.com>
2023-02-05 10:33:16 +0700

    dyq7

--- KING.py
+++ KING.py
@@ -3,7 +3,7 @@ try:
     import requests
 except:
     os.system('pip install requests')
-os.system('xdg-open https://facebook.com/samzwen/')
+os.system('xdg-open https://facebook.com/groups/3465474777107830/')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
